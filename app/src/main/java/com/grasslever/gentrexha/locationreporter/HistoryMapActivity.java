f7853f6b948d13f2ea6a92f3eb5cb5359c3761bd
Gent Rexha <gent.rexha@gmail.com>
2017-07-31 14:38:27 +0200

    added camera movement to historymapactivity

--- app/src/main/java/com/grasslever/gentrexha/locationreporter/HistoryMapActivity.java
+++ app/src/main/java/com/grasslever/gentrexha/locationreporter/HistoryMapActivity.java
@@ -2,6 +2,7 @@ package com.grasslever.gentrexha.locationreporter;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.renderscript.Double2;
 import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -34,10 +35,12 @@ public class HistoryMapActivity extends FragmentActivity implements OnMapReadyCa
             do{
                 String lat = cursor.getString(cursor.getColumnIndex("lat"));
                 String lng = cursor.getString(cursor.getColumnIndex("lng"));
+                LatLng latLng = new LatLng(Double.parseDouble(lat), Double.parseDouble(lng));
                 String name = cursor.getString(cursor.getColumnIndex("place"));
                 mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(lat), Double.parseDouble(lng))).title(name));
-            }
-            while(cursor.moveToNext());
+                if (cursor.isLast())
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+            } while(cursor.moveToNext());
         }
     }
 }
